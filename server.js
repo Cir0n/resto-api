72b9603accca524c5e93d79ea26964f1d96545a0
Cir0n <philippe.baba3000@gmail.com>
2026-02-14 23:29:53 +0100

    [DATABASE]-[CREATE]-Mise en place du fichier de configuration pour la DB, db.js

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const db = require('./db');
+
+
+async function testConnection() {
+  try {
+    const connection = await db.getConnection();
+    console.log('Database connection successful!');
+    connection.release();
+  } catch (error) {
+    console.error('Database connection failed:', error);
+  };
+}
+
+testConnection();
